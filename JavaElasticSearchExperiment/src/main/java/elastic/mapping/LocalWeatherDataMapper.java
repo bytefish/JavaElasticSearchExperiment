f0420d94c4db05a1bc64167e1734ece04c690e02
Philipp Wagner <bytefish@gmx.de>
2016-05-14 07:20:05 +0200

    Abstraction over Mapping added
    I am refactoring this project slowly into a more general framework for
    making Elasticsearch more accessible with Java. Refactoring Index
    Creation Mapping into separate concerns.

--- JavaElasticSearchExperiment/src/main/java/elastic/mapping/LocalWeatherDataMapper.java
+++ JavaElasticSearchExperiment/src/main/java/elastic/mapping/LocalWeatherDataMapper.java
@@ -3,18 +3,8 @@
 
 package elastic.mapping;
 
-import elastic.exceptions.GetMappingFailedException;
-import org.elasticsearch.Version;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.ToXContent;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.json.JsonXContent;
-import org.elasticsearch.index.mapper.ContentPath;
-import org.elasticsearch.index.mapper.Mapper;
-import org.elasticsearch.index.mapper.Mapping;
-import org.elasticsearch.index.mapper.Mapping.SourceTransform;
-import org.elasticsearch.index.mapper.MetadataFieldMapper;
 import org.elasticsearch.index.mapper.core.DateFieldMapper;
 import org.elasticsearch.index.mapper.core.FloatFieldMapper;
 import org.elasticsearch.index.mapper.core.StringFieldMapper;
@@ -22,27 +12,17 @@ import org.elasticsearch.index.mapper.geo.GeoPointFieldMapper;
 import org.elasticsearch.index.mapper.object.ObjectMapper;
 import org.elasticsearch.index.mapper.object.RootObjectMapper;
 
-import java.io.IOException;
-
-public class LocalWeatherDataMapper implements IObjectMapping {
+public class LocalWeatherDataMapper extends AbstractMap {
 
     private static final String INDEX_TYPE = "document";
 
-    public XContentBuilder getMapping() {
-        try {
-            return internalGetMapping();
-        } catch(Exception e) {
-            throw new GetMappingFailedException(INDEX_TYPE, e);
-        }
+    public LocalWeatherDataMapper() {
+        super(INDEX_TYPE);
     }
 
-    public String getIndexType() {
-        return INDEX_TYPE;
-    }
-
-    public XContentBuilder internalGetMapping() throws IOException {
-
-        RootObjectMapper.Builder rootObjectMapperBuilder = new RootObjectMapper.Builder(INDEX_TYPE)
+    @Override
+    protected void configure(RootObjectMapper.Builder builder) {
+        builder
                 .add(new DateFieldMapper.Builder("dateTime"))
                 .add(new FloatFieldMapper.Builder("temperature"))
                 .add(new FloatFieldMapper.Builder("windSpeed"))
@@ -57,19 +37,12 @@ public class LocalWeatherDataMapper implements IObjectMapping {
                                 .enableLatLon(true)
                                 .enableGeoHash(false))
                         .nested(ObjectMapper.Nested.newNested(true, false)));
+    }
 
-        Settings settings = Settings.builder()
+    @Override
+    protected void configure(Settings.Builder builder) {
+        builder
                 .put(IndexMetaData.SETTING_VERSION_CREATED, 1)
-                .put(IndexMetaData.SETTING_CREATION_DATE, System.currentTimeMillis())
-                .build();
-
-        Mapping mapping = new Mapping(
-                Version.fromString("1.0.0"),
-                rootObjectMapperBuilder.build(new Mapper.BuilderContext(settings, new ContentPath())),
-                new MetadataFieldMapper[] {},
-                new SourceTransform[] {},
-                null);
-
-        return mapping.toXContent(JsonXContent.contentBuilder().startObject(), ToXContent.EMPTY_PARAMS);
+                .put(IndexMetaData.SETTING_CREATION_DATE, System.currentTimeMillis());
     }
 }
